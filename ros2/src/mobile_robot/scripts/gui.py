9be6cbd8995f9332006478107f343625cb84ae1e
Ernesto Rincon <netorincon2@hotmail.com>
2023-05-18 21:46:22 +0200

    Modified GUI (added buttons, sliders, etc) and added logic to it. A publisher was created to obtain the sliders' position and publish it to cmd_robot topic
    This was done using ros2 multithreading libraries

---
+++ ros2/src/mobile_robot/scripts/gui.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os, sys
+import rclpy
+from PyQt5 import QtWidgets
+from threading import Thread 
+from widget import Ui_Form
+from slider_publisher import publisherNode
+from rclpy.executors import MultiThreadedExecutor
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+
+    pub_node = publisherNode(ui)
+    executor = MultiThreadedExecutor()
+    executor.add_node(pub_node)
+
+    # Start the ROS2 node on a separate thread
+    thread = Thread(target=executor.spin)
+    thread.start()
+    #print(type(thread.start()))
+    pub_node.get_logger().info("Spinned ROS2 Node . . .")
+
+    # Let the app running on the main thread
+    try:
+        Form.show()
+        sys.exit(app.exec_())
+    finally:
+        
+        pub_node.get_logger().info("Shutting down ROS2 Node . . .")
+        pub_node.destroy_node()
+        executor.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ ros2/src/mobile_robot/scripts/slider_publisher.py
@@ -0,0 +1,28 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from qwt.text import QwtTextLabel
+import os, signal
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from widget import Ui_Form
+from control_input.msg import ControlInput
+
+
+class publisherNode(Node):
+    def __init__(self, ui):
+        super().__init__('minimal_publisher')
+        self.ui = ui
+        self.publisher_ = self.create_publisher(ControlInput, 'cmd_robot', 10)
+        timer_period = 0.05  # seconds
+        self.i = 0
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+
+    def timer_callback(self):
+        msg = ControlInput()
+        msg.um=float(self.ui.umSlider.value()/100)
+        msg.beta1dot=float(self.ui.b1d_slider.value()/500)
+        msg.beta2dot=float(self.ui.b2d_slider.value()/500)
+        if(self.ui.manual_mode_radio.isChecked()):
+            self.publisher_.publish(msg)
+            self.i += 1
