3cf276b6abcf1946b6a68081231cf430ade8d8d2
kevinconda <kevingaitan0798@gmail.com>
2023-05-26 10:32:23 +0200

    Added position and velocity read test for 3 and 4

--- ros2/src/mobile_robot/src/motor_state.cpp
+++ ros2/src/mobile_robot/src/motor_state.cpp
@@ -506,12 +506,12 @@ class motor_state : public rclcpp::Node
 		}
 		
 		// Add parameter storage for Dynamixel#id present velocity
-		//dxl_addparam_result = velocityPacket.addParam(id, ADDR_PRESENT_VELOCITY, LEN_PV);
-		//if (dxl_addparam_result != true){
-			//fprintf(stderr, "[ID:%03d] grouBulkRead addparam failed (velocity)", id);
-			//exitParam = true;
-			////return;
-		//}
+		dxl_addparam_result = velocityPacket.addParam(id, ADDR_PRESENT_VELOCITY, LEN_PV);
+		if (dxl_addparam_result != true){
+			fprintf(stderr, "[ID:%03d] grouBulkRead addparam failed (velocity)", id);
+			exitParam = true;
+			//return;
+		}
 		
 		//Motors 3 and 4 (rotational) do not have torque control, there's no need to read the value
 		//However, we could technically read present load in the motors
@@ -585,22 +585,22 @@ class motor_state : public rclcpp::Node
 		stateArray[3].id = "4";
 		stateArray[3].position = positionPacket.getData(DXL4_ID, ADDR_PRESENT_POSITION, LEN_PV);
 		
-		//dxl_comm_result = velocityPacket.txRxPacket();
-		//if (dxl_comm_result != COMM_SUCCESS){
-			//printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-		//}
+		dxl_comm_result = velocityPacket.txRxPacket();
+		if (dxl_comm_result != COMM_SUCCESS){
+			printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+		}
 		//else if (velocityPacket.getError(DXL1_ID, &dxl_error)){
 			//printf("[ID:%03d] %s\n", DXL1_ID, packetHandler->getRxPacketError(dxl_error));
 		//}
 		//else if (velocityPacket.getError(DXL2_ID, &dxl_error)){
 			//printf("[ID:%03d] %s\n", DXL2_ID, packetHandler->getRxPacketError(dxl_error));
 		//}
-		//else if (velocityPacket.getError(DXL3_ID, &dxl_error)){
-			//printf("[ID:%03d] %s\n", DXL3_ID, packetHandler->getRxPacketError(dxl_error));
-		//}
-		//else if (velocityPacket.getError(DXL4_ID, &dxl_error)){
-			//printf("[ID:%03d] %s\n", DXL4_ID, packetHandler->getRxPacketError(dxl_error));
-		//}
+		else if (velocityPacket.getError(DXL3_ID, &dxl_error)){
+			printf("[ID:%03d] %s\n", DXL3_ID, packetHandler->getRxPacketError(dxl_error));
+		}
+		else if (velocityPacket.getError(DXL4_ID, &dxl_error)){
+			printf("[ID:%03d] %s\n", DXL4_ID, packetHandler->getRxPacketError(dxl_error));
+		}
 		
 		//readAvailable(velocityPacket, DXL1_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
 		//if(exitParam){
@@ -612,20 +612,20 @@ class motor_state : public rclcpp::Node
 			////return;
 		//}
 		
-		//readAvailable(velocityPacket, DXL3_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
-		//if(exitParam){
-			////return;
-		//}
+		readAvailable(velocityPacket, DXL3_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
+		if(exitParam){
+			//return;
+		}
 		
-		//readAvailable(velocityPacket, DXL4_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
-		//if(exitParam){
-			////return;
-		//}
+		readAvailable(velocityPacket, DXL4_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
+		if(exitParam){
+			//return;
+		}
 		
 		//stateArray[0].velocity = velocityPacket.getData(DXL1_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
 		//stateArray[1].velocity = velocityPacket.getData(DXL2_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
-		//stateArray[2].velocity = velocityPacket.getData(DXL3_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
-		//stateArray[3].velocity = velocityPacket.getData(DXL4_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
+		stateArray[2].velocity = velocityPacket.getData(DXL3_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
+		stateArray[3].velocity = velocityPacket.getData(DXL4_ID, ADDR_PRESENT_VELOCITY, LEN_PV);
 		
 		//dxl_comm_result = torquePacket.txRxPacket();
 		//if (dxl_comm_result != COMM_SUCCESS){
@@ -653,7 +653,7 @@ class motor_state : public rclcpp::Node
 
 		//printf("[ID:%03d] \n Present Position : %.2f \t Present Velocity : %.2f \t Present Torque : %.2f \n [ID:%03d] \n Present Position : %.2f \t Present Velocity : %.2f \t Present Torque : %.2f \n [ID:%03d] \n Present Position : %.2f \t Present Velocity : %.2f \n [ID:%03d] \n Present Position : %.2f \t Present Velocity : %.2f \n", DXL1_ID, stateArray[0].position, stateArray[0].velocity, stateArray[0].torque, DXL2_ID, stateArray[1].position, stateArray[1].velocity, stateArray[1].torque, DXL3_ID, stateArray[2].position, stateArray[2].velocity, DXL4_ID, stateArray[3].position, stateArray[3].velocity);
 		
-		printf("\n[ID:%03d] \n Present Position : %.2f \t\n [ID:%03d] \n Present Position : %.2f \t\n", DXL3_ID, stateArray[2].position, DXL4_ID, stateArray[3].position);
+		printf("\n[ID:%03d] \n Present Position : %.2f \t Present Velocity : %.2f \t\n [ID:%03d] \n Present Position : %.2f \t Present Velocity : %.2f \t\n", DXL3_ID, stateArray[2].position, stateArray[2].velocity, DXL4_ID, stateArray[3].position, stateArray[3].velocity);
 	}
 
 	void paramStorageWrite(uint8_t id, int address, int len, uint8_t param_goal_position[4]){
