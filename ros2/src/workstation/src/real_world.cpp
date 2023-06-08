6b4502c4375ff1fc4db662de9346e675f8c47384
Ernesto Rincon <netorincon2@hotmail.com>
2023-06-08 10:31:03 +0200

    Added tt_dd calculation

--- ros2/src/workstation/src/real_world.cpp
+++ ros2/src/workstation/src/real_world.cpp
@@ -206,17 +206,17 @@ class real_world : public rclcpp::Node
         st=sin(tt);
         float aux1=0, aux2=0, aux3=0;
 
-        aux1 = mass*(4*pow(R,2)*sd1*st*ct*dd1 -4*pow(R,2)*sd1*pow(st, 2)*cd1*dd1*phi1dCmd + 4*pow(R,2)*sd1*cd1*pow(ct,2)*dd1*phi1dCmd
-                        +4*pow(R,2)*sd2*pow(st,2)*cd1*phi2dCmd*tt_dot - 4*pow(R,2)*sd2*st*cd1*ct*phi2dd -4*pow(R,2)*sd2*cd1*pow(ct,2)*phi2dCmd*tt_dot
-                        +2*pow(R,2)*pow(st,2)*pow(cd1,2)*phi1dd - 4*pow(R,2)*st*cd1*cd2*ct*dd2*phi2dCmd + 2*pow(R,2)*pow(cd1,2)*pow(ct,2)*phi1dd);
+        // aux1 = mass*(4*pow(R,2)*sd1*st*ct*dd1 -4*pow(R,2)*sd1*pow(st, 2)*cd1*dd1*phi1dCmd + 4*pow(R,2)*sd1*cd1*pow(ct,2)*dd1*phi1dCmd
+        //                 +4*pow(R,2)*sd2*pow(st,2)*cd1*phi2dCmd*tt_dot - 4*pow(R,2)*sd2*st*cd1*ct*phi2dd -4*pow(R,2)*sd2*cd1*pow(ct,2)*phi2dCmd*tt_dot
+        //                 +2*pow(R,2)*pow(st,2)*pow(cd1,2)*phi1dd - 4*pow(R,2)*st*cd1*cd2*ct*dd2*phi2dCmd + 2*pow(R,2)*pow(cd1,2)*pow(ct,2)*phi1dd);
 
-        aux2 = (pow(R,2)*l1/(2*a))*(-2*pow(sd1,2)*ct*dd1*phi1dCmd + sd1*sd2*ct*dd1*phi2dCmd - 2*sd1*st*cd1*phi1dCmd*tt_dot + 2*sd1*cd1*ct*phi1dd 
-                                    -pow(sd2,2)*st*phi2dd - pow(sd2,2)*ct*phi2dCmd*tt_dot + sd2*st*cd1*phi2dCmd*tt_dot - 2*sd2*st*cd2*dd2*phi2dCmd
-                                    - sd2*cd1*ct*phi2dd + 2*pow(cd1,2)*ct*dd1*phi1dCmd - cd1*cd2*ct*dd2*phi2dCmd);
+        // aux2 = (pow(R,2)*l1/(2*a))*(-2*pow(sd1,2)*ct*dd1*phi1dCmd + sd1*sd2*ct*dd1*phi2dCmd - 2*sd1*st*cd1*phi1dCmd*tt_dot + 2*sd1*cd1*ct*phi1dd 
+        //                             -pow(sd2,2)*st*phi2dd - pow(sd2,2)*ct*phi2dCmd*tt_dot + sd2*st*cd1*phi2dCmd*tt_dot - 2*sd2*st*cd2*dd2*phi2dCmd
+        //                             - sd2*cd1*ct*phi2dd + 2*pow(cd1,2)*ct*dd1*phi1dCmd - cd1*cd2*ct*dd2*phi2dCmd);
         
-        aux3 = (zz/pow(2*a, 2))*(pow(R,2)*pow(sd1,2)*phi1dd - pow(R,2)*sd1*sd2*phi2dd + 2*pow(R,2)*sd1*cd1*dd1*phi1dCmd - pow(R,2)*sd1*cd2*dd2*phi2dCmd - pow(R,2)*sd2*cd1*dd1*phi2dCmd);
+        // aux3 = (zz/pow(2*a, 2))*(pow(R,2)*pow(sd1,2)*phi1dd - pow(R,2)*sd1*sd2*phi2dd + 2*pow(R,2)*sd1*cd1*dd1*phi1dCmd - pow(R,2)*sd1*cd2*dd2*phi2dCmd - pow(R,2)*sd2*cd1*dd1*phi2dCmd);
 
-        t1Cmd=(aux1+aux2+aux3)*0.5;
+        // t1Cmd=(aux1+aux2+aux3)*0.5;
 
         joint_cmd.name.push_back(names[0]);
         joint_cmd.name.push_back(names[2]);
@@ -226,11 +226,12 @@ class real_world : public rclcpp::Node
 
         joint_cmd.name.push_back(names[1]);
         //joint_cmd.velocity.push_back(phi1dCmd);
-        joint_cmd.effort.push_back(t1Cmd);
-        publishJointCommand();
+        joint_cmd.effort.push_back(phi2dCmd);
+        //publishJointCommand();
 
         joint_cmd.name.push_back(names[3]);
-        joint_cmd.velocity.push_back(phi2dCmd);
+        //joint_cmd.velocity.push_back(phi2dCmd);
+        joint_cmd.effort.push_back(phi1dCmd);
         publishJointCommand();
     }
 
