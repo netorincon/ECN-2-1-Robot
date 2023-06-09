627c1fdd24a92d3a7332f51ffdaa2db2e3d11907
Ernesto Rincon <netorincon2@hotmail.com>
2023-06-09 11:01:09 +0200

    Updated service call

--- ros2/src/workstation/scripts/widget.py
+++ ros2/src/workstation/scripts/widget.py
@@ -62,7 +62,7 @@ class Ui_Form(object):
             os.kill(self.launchP.processId(), signal.SIGINT)
             self.launchP.waitForFinished(-1)
             if(self.real_world_radio.isChecked()):
-                os.system("ros2 service call /motor_state/reset_robot_state control_input/srv/ResetRobot")
+                os.system("ros2 service call /robot_state/reset_robot_state control_input/srv/ResetRobot")
             elif(self.simulation_radio.isChecked()):
                 os.system("ros2 service call /sim/reset_robot_state control_input/srv/ResetRobot")
         time.sleep(0.5)
