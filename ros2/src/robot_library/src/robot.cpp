7e62fd73348780cd558634a330fea74168cf3d1f
Ernesto Rincon <netorincon2@hotmail.com>
2023-06-13 14:30:31 +0200

    Update Robot class (limit_angle)

--- ros2/src/robot_library/src/robot.cpp
+++ ros2/src/robot_library/src/robot.cpp
@@ -101,8 +101,8 @@ Eigen::Matrix2f Robot::getKInv(float e){
 }
 
 float Robot::limit_angle(float angle){
-    while( angle >=  2*M_PI ) angle -= 2*M_PI ;
-    while( angle <  0 ) angle += 2*M_PI ;
+    while( angle >=  M_PI ) angle -= 2*M_PI ;
+    while( angle <  -M_PI ) angle += 2*M_PI ;
     return angle;
 }
 
